9fb4ddd5cf46972b5b214e5351f397040c0dec43
Shalom Naim <snaim@redhat.com>
2017-07-24 10:21:44 +0300

    add default username

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 def main():
     parser = argparse.ArgumentParser(description='Ansible inventory creator')
     parser.add_argument('--ip', action='store', dest="ip", help="IP address of target server")
-    parser.add_argument('--username', action='store', dest="username", help="username of target server")
+    parser.add_argument('--username', action='store',default="root", dest="username", help="username of target server")
     parser.add_argument('--password', action='store', dest="password", help="password of target server")
     args = parser.parse_args()
     try:
@@ -20,14 +20,11 @@ def main():
 
     console = pexpect.spawn("ssh-copy-id {user}@{ip}".format(user=args.username, ip=args.ip))
     console.logfile = sys.stdout
-    print console.before
     console.expect(" (yes/no)?")
     console.send("yes\n")
     console.expect("password:")
     console.send("{password}\n".format(password=args.password))
     console.expect("you wanted were added")
-    print "************\n\n"
-    print console.before
     print console.after
 
 if __name__ == "__main__":
