c44a8b3a612ec5409d48d42e0184ac50fbc87dbf
Shalom Naim <snaim@redhat.com>
2017-07-24 10:27:13 +0300

    splite code to functions

--- main.py
+++ main.py
@@ -7,25 +7,39 @@ import sys
 def main():
     parser = argparse.ArgumentParser(description='Ansible inventory creator')
     parser.add_argument('--ip', action='store', dest="ip", help="IP address of target server")
-    parser.add_argument('--username', action='store',default="root", dest="username", help="username of target server")
+    parser.add_argument('--username', action='store', default="root", dest="username", help="username of target server")
     parser.add_argument('--password', action='store', dest="password", help="password of target server")
     args = parser.parse_args()
+
+    if not validate_params(args):
+        parser.print_help()
+        sys.exit(1)
+
+    swap_keys(args.ip, args.username, args.password)
+
+def validate_params(args):
+    is_valid = True
+
     try:
         assert isinstance(args.ip, str), "No IP was supplied"
         assert isinstance(args.username, str), "No username was supplied"
         assert isinstance(args.password, str), "No password was supplied"
     except Exception as ex:
         print ex.message
-        parser.print_help()
+        is_valid = False
+    finally:
+        return is_valid
 
-    console = pexpect.spawn("ssh-copy-id {user}@{ip}".format(user=args.username, ip=args.ip))
+def swap_keys(ip, username, password):
+    console = pexpect.spawn("ssh-copy-id {user}@{ip}".format(user=username, ip=ip))
     console.logfile = sys.stdout
     console.expect(" (yes/no)?")
     console.send("yes\n")
     console.expect("password:")
-    console.send("{password}\n".format(password=args.password))
+    console.send("{password}\n".format(password=password))
     console.expect("you wanted were added")
     print console.after
+
 
 if __name__ == "__main__":
     main()
